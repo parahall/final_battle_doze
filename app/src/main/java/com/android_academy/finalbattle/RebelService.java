b6f188416dc1e2300802f8f2e6627c1cc07bfb8e
Yonatan Levin <parahall@gmail.com>
2016-09-03 20:15:23 +0300

    Foreground service

--- app/src/main/java/com/android_academy/finalbattle/RebelService.java
+++ app/src/main/java/com/android_academy/finalbattle/RebelService.java
@@ -1,20 +1,25 @@
 package com.android_academy.finalbattle;
 
+import android.app.Notification;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.IBinder;
 import android.os.Looper;
 import android.os.Message;
+import android.os.PowerManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class RebelService extends Service implements Handler.Callback {
   private static final String TAG = RebelService.class.getSimpleName();
   private static final int WHAT_MAKE_NETWORK_REQUEST = 101;
-  private static final int DELAY_MILLIS = 30 * 1000;
+  private static final int DELAY_MILLIS = 5 * 1000;
   private Handler handler;
   private HandlerThread handlerThread;
+  private PowerManager.WakeLock wl;
 
   public RebelService() {
   }
@@ -31,6 +36,17 @@ public class RebelService extends Service implements Handler.Callback {
     Looper looper = handlerThread.getLooper();
     handler = new Handler(looper, this);
     handler.sendEmptyMessage(WHAT_MAKE_NETWORK_REQUEST);
+    PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+    wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
+    wl.acquire();
+
+    Notification notification =
+        new NotificationCompat.Builder(this).setContentTitle("Truiton Music Player")
+            .setTicker("Rebel Services")
+            .setContentText("To protect and save the galaxy")
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .build();
+    startForeground(101, notification);
   }
 
   @Override public boolean handleMessage(Message msg) {
@@ -42,6 +58,7 @@ public class RebelService extends Service implements Handler.Callback {
   }
 
   @Override public void onDestroy() {
+    wl.release();
     handler.removeCallbacksAndMessages(null);
     handlerThread.quit();
     handler = null;
